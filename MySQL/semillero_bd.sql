da4e23b3bb090d1aa891648b19b0cceeb820718b
Santiago Betancur Graciano <santybg_94@hotmail.com>
2021-01-16 23:23:12 -0500

    Starting the work with mysql Jan 16th

--- MySQL/semillero_bd.sql
+++ MySQL/semillero_bd.sql
@@ -1,25 +1,40 @@
+--GRANT ALL PRIVILEGES ON semillero.* TO 'santiago'@'localhost';
+
+--CREATE DATABASE semillero;
+
+USE semillero;
+
+-- CREATION OF TABLES
 DROP TABLE IF EXISTS vehiculos;
 DROP TABLE IF EXISTS tipo_linea;
 DROP TABLE IF EXISTS tipo_marca;
+DROP TABLE IF EXISTS modelo;
 
 CREATE TABLE vehiculos(
-    nro_placa VARCHAR(6) PRIMARY KEY,
-    id_linea INT(5) FOREIGN KEY,
-    modelo,
-    fecha_ven_seguro TIMESTAMP,
-    fecha_ven_tecnomecanica TIMESTAMP,
-    fecha_ven_contratodo TIMESTAMP
+    id_vehiculo INT(5) PRIMARY KEY AUTO_INCREMENT,
+    nro_placa VARCHAR(8) NOT NULL,
+    id_linea INT(5) NOT NULL,
+    id_modelo INT(5) NOT NULL,
+    fecha_ven_seguro DATE NOT NULL,
+    fecha_ven_tecnomecanica DATE NOT NULL,
+    fecha_ven_contratodo DATE NOT NULL
 );
 
 CREATE TABLE tipo_linea(
     id_linea INT(5) PRIMARY KEY AUTO_INCREMENT,
-    desc_linea INT(5) NULL,
-    id_marca FOREIGN KEY INT(10),
-    activo ENUM('S','N')
+    desc_linea VARCHAR(255) COMMENT 'Descripción de la línea',
+    id_marca INT(10) NOT NULL,
+    activo ENUM('S','N') NOT NULL
 );
 
 CREATE TABLE tipo_marca(
     id_marca INT(5) PRIMARY KEY AUTO_INCREMENT,
-    desc_marca INT(5) NULL,
-    activo ENUM('S','N')
+    desc_marca VARCHAR(255) COMMENT 'Descripción de la marca',
+    activo ENUM('S','N') NOT NULL
+);
+
+CREATE TABLE modelo(
+    id_modelo INT(5) PRIMARY KEY AUTO_INCREMENT,
+    year_modelo INT(5) NOT NULL,
+    desc_modelo VARCHAR(255) COMMENT 'Descripción del modelo'
 );
